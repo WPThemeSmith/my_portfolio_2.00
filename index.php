6a32c6ec01f61a768485c4e68c8d56cbe0a81c0a
MD Rakibul Islam <mtsrakibul@gmail.com>
2025-05-12 23:23:54 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<head>
+    <?php include_once './includes/head.php';?>
+</head>
+
+<body class="index-page">
+
+    <header>
+        <?php require_once './includes/header.php'; ?>
+    </header>
+
+  <main class="main">
+
+    <!-- Hero Section -->
+    <section>
+        <?php include_once './includes/hero.php';?>
+    </section><!-- /Hero Section -->
+
+    <!-- About Section -->
+    <section id="about" class="about section">
+        <?php include_once './pages/about.php';?>
+    </section><!-- /About Section -->
+
+    <!-- Stats Section -->
+    <section id="stats" class="stats section">
+        <?php include_once './pages/stats.php';?>
+    </section><!-- /Stats Section -->
+
+    <!-- Skills Section -->
+    <section id="skills" class="skills section">
+        <?php include_once './pages/skills.php';?>
+    </section><!-- /Skills Section -->
+
+    <!-- Resume Section -->
+    <section id="resume" class="resume section">
+        <?php include_once './pages/resume.php';?>
+    </section><!-- /Resume Section -->
+
+    <!-- Portfolio Section -->
+    <section id="portfolio" class="portfolio section">
+        <?php include_once './pages/portfolio.php';?>
+    </section><!-- /Portfolio Section -->
+
+    <!-- Services Section -->
+    <section id="services" class="services section">
+        <?php include_once './pages/services.php';?>
+    </section><!-- /Services Section -->
+
+    <!-- Testimonials Section -->
+    <section id="testimonials" class="testimonials section">
+        <?php include_once './pages/testimonials.php';?>
+    </section><!-- /Testimonials Section -->
+
+    <!-- Contact Section -->
+    <section id="contact" class="contact section">
+        <?php include_once './pages/contact-us.php';?>
+    </section><!-- /Contact Section -->
+
+  </main>
+
+  <footer>
+    <?php include_once './includes/footer.php';?>
+  </footer>
+  
+</body>  
