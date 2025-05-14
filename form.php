874639d59d7fa8233a77e387abccf4a079d083f3
MD Rakibul Islam <mtsrakibul@gmail.com>
2025-05-14 14:14:15 +0600

    form changes

---
+++ form.php
@@ -0,0 +1,23 @@
+<?php
+$fullname =(!empty($_POST["fullname"])) ? $_POST["fullname"] : null;
+
+$email = (!empty($_POST["email"])) ? $_POST["email"] : null;
+$phone = $_POST["phone"];
+$subject = $_POST["subject"];
+$message =$_POST["message"];
+
+if(empty($fullname) || ($email) || ($phone)){
+    echo "You need to fill-up all the fields";
+}
+exit;
+echo "Name: $fullname <br>";
+echo "Email: $email <br>";
+echo "Phone: $phone <br>";
+echo "Subject: $subject <br>";
+echo "Message: $message <br>";
+
+
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+?>
--- pages/contact-us.php
+++ pages/contact-us.php
@@ -39,23 +39,27 @@
           </div>
 
           <div class="col-lg-8">
-            <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="200">
+            <form action="form.php" method="POST" class="php-email-form" data-aos="fade-up" data-aos-delay="200">
               <div class="row gy-4">
 
                 <div class="col-md-6">
-                  <input type="text" name="name" class="form-control" placeholder="Your Name" required="">
+                  <input type="text" name="fullname" class="form-control" placeholder="Your Name">
                 </div>
 
                 <div class="col-md-6 ">
-                  <input type="email" class="form-control" name="email" placeholder="Your Email" required="">
+                  <input type="email" class="form-control" name="email" placeholder="Your Email">
+                </div>
+
+                <div class="col-md-6 ">
+                  <input type="tel" class="form-control" name="phone" placeholder="Your Phone Number">
+                </div>
+
+                <div class="col-md-6">
+                  <input type="text" class="form-control" name="subject" placeholder="Subject">
                 </div>
 
                 <div class="col-md-12">
-                  <input type="text" class="form-control" name="subject" placeholder="Subject" required="">
-                </div>
-
-                <div class="col-md-12">
-                  <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
+                  <textarea class="form-control" name="message" rows="6" placeholder="Message"></textarea>
                 </div>
 
                 <div class="col-md-12 text-center">
